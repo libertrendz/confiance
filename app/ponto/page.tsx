a18ba846ed03153849841fe12faee7222d9c1822
libertrendz <gustavo.santos@libertrendz.com.br>
2025-10-07 13:41:13 +0100

    Update page.tsx

--- app/ponto/page.tsx
+++ app/ponto/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
 
 import { useEffect, useMemo, useState } from "react";
 import CameraPunch, { type PunchPayload } from "../../components/CameraPunch";
