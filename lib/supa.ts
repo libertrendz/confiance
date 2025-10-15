f23ec5e54a2ae68832657525aab26ccebb5e6c22
libertrendz <gustavo.santos@libertrendz.com.br>
2025-10-15 11:31:18 +0100

    Update supa.ts

--- lib/supa.ts
+++ lib/supa.ts
@@ -1,7 +1,14 @@
-'use client';
-import { createBrowserClient } from '@supabase/ssr';
+// lib/supa.ts
+import { createClient } from '@supabase/supabase-js'
 
-export const supa = createBrowserClient(
+export const supa = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: false, // vamos tratar o callback manualmente
+    },
+  }
+)
