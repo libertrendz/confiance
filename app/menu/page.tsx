6b0d5bda5a83ed12d264a52890bc7701a7ea73de
libertrendz <gustavo.santos@libertrendz.com.br>
2025-10-15 15:13:40 +0100

    Update page.tsx

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { supabase } from '@/lib/supabaseClient'
 import { supa } from '../../lib/supa'
 
 export default function Menu() {
