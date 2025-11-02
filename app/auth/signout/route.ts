5c6bf9cf3a1fec1e938bc275d9d846c09296bad2
Libertrendz <gustavo.santos@libertrendz.com.br>
2025-11-02 19:23:26 +0000

    Update route.ts

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -1,6 +1,7 @@
 // app/auth/signout/route.ts
+// app/auth/signout/route.ts
 import { NextResponse } from 'next/server';
-import { getServerSupabase } from '@/lib/supabaseServer';
+import { getServerSupabase } from '../../../lib/supabaseServer'; // <-- relativo
 
 export async function GET(req: Request) {
   const supa = getServerSupabase();
