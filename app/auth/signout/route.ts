c81f6720c6bfe7a3cc5367bb4af50905332aad28
Libertrendz <gustavo.santos@libertrendz.com.br>
2025-11-03 14:03:39 +0000

    Update route.ts

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -1,9 +1,8 @@
-// app/auth/signout/route.ts
 import { NextResponse } from 'next/server';
-import { getServerSupabase } from '@/lib/supabaseServer';
+import { getServerSupabase } from '../../../lib/supabaseServer';
 
 export async function GET(req: Request) {
   const supa = getServerSupabase();
-  await supa.auth.signOut(); // limpa cookies http-only
+  await supa.auth.signOut();
   return NextResponse.redirect(new URL('/login', new URL(req.url).origin));
 }
