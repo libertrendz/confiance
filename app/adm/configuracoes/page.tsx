fa1ec4a58a1cffcaefe051083921772e32ff0d2d
Libertrendz <gustavo.santos@libertrendz.com.br>
2025-11-08 00:10:10 +0000

    Create page.tsx

---
+++ app/adm/configuracoes/page.tsx
@@ -0,0 +1,8 @@
+export default function ConfigPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: 22, fontWeight: 800 }}>Configurações</h1>
+      <p style={{ color: '#49546A' }}>Em construção. Preferências da empresa e branding.</p>
+    </div>
+  );
+}
