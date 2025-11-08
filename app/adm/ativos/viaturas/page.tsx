db7b301141c407af454e4e8ae4a718b17a01910a
Libertrendz <gustavo.santos@libertrendz.com.br>
2025-11-08 00:11:58 +0000

    Create page.tsx

---
+++ app/adm/ativos/viaturas/page.tsx
@@ -0,0 +1,10 @@
+export default function ViaturasPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: 22, fontWeight: 800 }}>Viaturas</h1>
+      <p style={{ color: '#49546A' }}>
+        Em construção. Checklist, quilometragem, manutenção, custos e alocação.
+      </p>
+    </div>
+  );
+}
