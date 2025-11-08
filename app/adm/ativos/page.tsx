96c2aec55b6fbe8b0cdba400d63da047f8cdbd31
Libertrendz <gustavo.santos@libertrendz.com.br>
2025-11-08 00:10:41 +0000

    Create page.tsx

---
+++ app/adm/ativos/page.tsx
@@ -0,0 +1,10 @@
+export default function AtivosPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: 22, fontWeight: 800 }}>Ativos</h1>
+      <p style={{ color: '#49546A' }}>
+        Gestão central de ativos. Use os subitens “Ferramentas” e “Viaturas” no menu.
+      </p>
+    </div>
+  );
+}
