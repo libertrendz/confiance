27d9f9c54d1e38efc85d1d411d37ecd93ecffd15
Libertrendz <gustavo.santos@libertrendz.com.br>
2025-11-08 00:11:23 +0000

    Create page.tsx

---
+++ app/adm/ativos/ferramentas/page.tsx
@@ -0,0 +1,10 @@
+export default function FerramentasPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: 22, fontWeight: 800 }}>Ferramentas</h1>
+      <p style={{ color: '#49546A' }}>
+        Em construção. Controle de estoque, alocação por colaborador, localização e manutenção.
+      </p>
+    </div>
+  );
+}
