55b4d36daf102e88391013cd8b9287356d9827a6
Libertrendz <gustavo.santos@libertrendz.com.br>
2025-11-13 11:15:53 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,23 +3,13 @@ import './globals.css';
 
 export const metadata = {
   title: 'CONFIANCE',
-  description: 'ERP modular leve',
+  description: 'App CONFIANCE',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt">
-      <body>
-        {children}
-        <footer style={{
-          textAlign: 'center',
-          fontSize: 12,
-          color: 'var(--muted)',
-          padding: '12px 0'
-        }}>
-          Powered by <strong>LIBERTRENDZ®</strong>
-        </footer>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
