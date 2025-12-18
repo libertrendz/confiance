b34d6d8d87122ab28ad6394cafbfcd4795928a75
Libertrendz <gustavo.santos@libertrendz.com.br>
2025-12-18 21:30:14 +0000

    Update layout.tsx

--- app/adm/layout.tsx
+++ app/adm/layout.tsx
@@ -148,7 +148,7 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
 
           {(isAdmin || isGestor) && (
             <a href="/adm/ponto" style={linkStyle}>
-              Ponto (ADM)
+              Registo de Ponto
             </a>
           )}
 
