cc494d43f43b3e03daac7617e8b030796a04ba8f
Libertrendz <gustavo.santos@libertrendz.com.br>
2025-12-23 21:33:51 +0000

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -175,7 +175,7 @@ export default function LoginPage() {
           {/* Powered by (NÃO some) */}
           <div style={{ display: 'grid', justifyItems: 'center', marginTop: 18 }}>
             <img
-              src="/powered_by_libertrendz.png"
+              src="/powered-by-libertrendz.png"
               alt="Powered by Libertrendz"
               style={{
                 width: 'min(260px, 70vw)',
